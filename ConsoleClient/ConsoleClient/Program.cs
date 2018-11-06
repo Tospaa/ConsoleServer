a61b3fc83bfc50e0eac406b21fb847ca86a5cdf7
Tospaa <musaecer@gmail.com>
2018-11-06 15:41:55 +0300

    Client now has Ear!!
    Our beloved client can now listen the environment around it!

--- ConsoleClient/ConsoleClient/Program.cs
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleClient
 {
@@ -13,6 +14,8 @@ namespace ConsoleClient
         const string SERVER_IP = "127.0.0.1";
         static void Main(string[] args)
         {
+            Thread kulak = new Thread(new ThreadStart(Kulak));
+            kulak.Start();
             //---data to send to the server---
             string textToSend;
             NetworkStream nwStream;
@@ -28,7 +31,7 @@ namespace ConsoleClient
                     Console.ReadKey(true);
                     continue;
                 }
-
+                Console.WriteLine("Connection established.");
                 while (true)
                 {
                     try { 
@@ -41,16 +44,48 @@ namespace ConsoleClient
                     catch (System.IO.IOException)
                     {
                        Console.WriteLine("Connection lost. Will try to connect again.");
+                       break;
+                    }
+                }
+            }
+        }
+
+        private static void Kulak() {
+            TcpListener sunucu = new TcpListener(IPAddress.Parse(SERVER_IP), PORT_NO + 1);
+            sunucu.Start();
+            Console.WriteLine("Ear is listening on {0}:{1}", SERVER_IP, PORT_NO + 1);
+
+            while (true)
+            {
+                TcpClient istemci = sunucu.AcceptTcpClient();
+                Console.WriteLine("Ear stumbled upon a stranger...");
+                NetworkStream yayin = istemci.GetStream();
+
+                while (true)
+                {
+                    try
+                    {
+                        Byte[] gelen_ham_baytlar = new Byte[istemci.Available];
+                        yayin.Read(gelen_ham_baytlar, 0, gelen_ham_baytlar.Length);
+
+                        //translate bytes of request to string
+                        String gelen_veri = Encoding.UTF8.GetString(gelen_ham_baytlar);
+                        if (gelen_veri.Length > 0)
+                        {
+                            Console.WriteLine("Stranger: {0}", gelen_veri);
+                            if (gelen_veri == "Result: 1")
+                            {
+                                Environment.Exit(1);
+                            }
+                        }
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        Console.WriteLine("Stranger left.");
                         break;
                     }
                 }
             }
-            //---read back the text---
-            //byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-            //int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-            //Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-            //Console.ReadLine();
-            //client.Close();
         }
     }
 }
